9d3cdefd649a19f7b5bc638f40511b0041b35140
Deep Patel <deeppatel2667@gmail.com>
2021-09-19 22:50:10 -0400

    Dino Game without AI

--- DinoGame.py
+++ DinoGame.py
@@ -1,243 +1,330 @@
 import pygame
 import os
-import sys
 import random
-import math
-import neat
+pygame.font.init()
 
-pygame.init()
+WIDTH, HEIGHT = 700, 400
+DINO_WIDTH, DINO_HEIGHT = 63, 63
 
-SCREEN_HEIGHT = 600
-SCREEN_WIDTH = 1100
-SCREEN = pygame.display.set_mode(((SCREEN_WIDTH, SCREEN_HEIGHT)))
+BIRD_WIDTH, BIRD_HEIGHT = 63, 50
 
-RUNNING = [pygame.image.load(os.path.join("Assets1", "DinoRun1.png")),
-           pygame.image.load(os.path.join("Assets1", "DinoRun2.png"))]
+DINO_Y = HEIGHT//2 + 48
 
-JUMPING = pygame.image.load(os.path.join("Assets1", "DinoJump.png"))
+ONE_CACTUS_TALL_WIDTH, ONE_CACTUS_TALL_HEIGHT = 25, 50
+ONE_CACTUS_SMALL_WIDTH, ONE_CACTUS_SMALL_HEIGHT = 15, 30
 
-SMALL_CACTUS = [pygame.image.load(os.path.join("Assets1", "SmallCactus1.png")),
-                pygame.image.load(os.path.join("Assets1", "SmallCactus2.png")),
-                pygame.image.load(os.path.join("Assets1", "SmallCactus3.png"))]
-LARGE_CACTUS = [pygame.image.load(os.path.join("Assets1", "LargeCactus1.png")),
-                pygame.image.load(os.path.join("Assets1", "LargeCactus2.png")),
-                pygame.image.load(os.path.join("Assets1", "LargeCactus3.png"))]
+TWO_CACTUS_TALL_WIDTH, TWO_CACTUS_TALL_HEIGHT = 51, 50
+TWO_CACTUS_SMALL_WIDTH, TWO_CACTUS_SMALL_HEIGHT = 36, 35
 
+FOUR_CACTUS_TALL_WIDTH, FOUR_CACTUS_TALL_HEIGHT = 76, 50
+FOUR_CACTUS_SMALL_WIDTH, FOUR_CACTUS_SMALL_HEIGHT = 53, 35
 
-BG = pygame.image.load(os.path.join("Assets1", "Track.png"))
+FPS = 60
+BACKGROUND_VEL = 1
 
-FONT = pygame.font.SysFont('comicsans', 20)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (83, 83, 83)
 
-class Dinosaur:
-    X_POS = 80
-    Y_POS = 310
-    JUMP_VEL = 8.5
+DINO_IMAGE = pygame.image.load(os.path.join('Assets', 'Dino.png'))
+DINO1_IMAGE = pygame.image.load(os.path.join('Assets', 'dino1.png'))
+DINO2_IMAGE = pygame.image.load(os.path.join('Assets', 'dino2.png'))
+DINO = pygame.transform.scale(DINO_IMAGE, (DINO_WIDTH, DINO_HEIGHT))
+DINO1 = pygame.transform.scale(DINO1_IMAGE, (DINO_WIDTH, DINO_HEIGHT))
+DINO2 = pygame.transform.scale(DINO2_IMAGE, (DINO_WIDTH, DINO_HEIGHT))
 
-    def __init__(self, img=RUNNING[0]):
-        self.image = img
-        self.dino_run = True
-        self.dino_Bigjump = False
-        self.dino_Smalljump = False
-        self.jump_vel = self.JUMP_VEL
-        self.rect = pygame.Rect(self.X_POS, self.Y_POS, img.get_width(), img.get_height())
-        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-        self.step_index = 0
+BIRD_IMAGE = pygame.image.load(os.path.join('Assets', 'bird.png'))
+BIRD2_IMAGE = pygame.image.load(os.path.join('Assets', 'bird2.png'))
+BIRD = pygame.transform.scale(BIRD_IMAGE, (BIRD_WIDTH, BIRD_HEIGHT))
+BIRD2 = pygame.transform.scale(BIRD2_IMAGE, (BIRD_WIDTH, BIRD_HEIGHT))
 
-    def update(self):
-        if self.dino_run:
-            self.run()
-        if self.dino_Bigjump:
-            self.Bigjump()
-        if self.dino_Smalljump:
-            self.Smalljump()
-        if self.step_index >= 10:
-            self.step_index = 0
+ONE_CACTUS_IMAGE = pygame.image.load(os.path.join('Assets', 'OneCactus.png'))
+ONE_CACTUS_TALL = pygame.transform.scale(ONE_CACTUS_IMAGE, (ONE_CACTUS_TALL_WIDTH, ONE_CACTUS_TALL_HEIGHT))
+ONE_CACTUS_SMALL = pygame.transform.scale(ONE_CACTUS_IMAGE, (ONE_CACTUS_SMALL_WIDTH, ONE_CACTUS_SMALL_HEIGHT))
 
+TWO_CACTUS_IMAGE = pygame.image.load(os.path.join('Assets', 'TwoCactus.png'))
+TWO_CACTUS_TALL = pygame.transform.scale(TWO_CACTUS_IMAGE, (TWO_CACTUS_TALL_WIDTH, TWO_CACTUS_TALL_HEIGHT))
+TWO_CACTUS_SMALL = pygame.transform.scale(TWO_CACTUS_IMAGE, (TWO_CACTUS_SMALL_WIDTH, TWO_CACTUS_SMALL_HEIGHT))
 
-    def Bigjump(self):
-        self.image = JUMPING
-        if self.dino_Bigjump:
-            self.rect.y -= self.jump_vel * 4
-            self.jump_vel -= 0.8
-        if self.jump_vel <= -self.JUMP_VEL:
-            self.dino_Bigjump = False
-            self.dino_run = True
-            self.jump_vel = self.JUMP_VEL
+FOUR_CACTUS_IMAGE = pygame.image.load(os.path.join('Assets', 'FourCactus.png'))
+FOUR_CACTUS_TALL = pygame.transform.scale(FOUR_CACTUS_IMAGE, (FOUR_CACTUS_TALL_WIDTH, FOUR_CACTUS_TALL_HEIGHT))
+FOUR_CACTUS_SMALL = pygame.transform.scale(FOUR_CACTUS_IMAGE, (FOUR_CACTUS_SMALL_WIDTH, FOUR_CACTUS_SMALL_HEIGHT))
 
-    def Smalljump(self):
-        self.image = JUMPING
-        if self.dino_Smalljump:
-            self.rect.y -= self.jump_vel * 4
-            self.jump_vel -= 1.0
-        if self.jump_vel <= -self.JUMP_VEL:
-            self.dino_Smalljump = False
-            self.dino_run = True
-            self.jump_vel = self.JUMP_VEL
+BACKGROUND = pygame.image.load(os.path.join('Assets', 'Background.png'))
+BACKGROUND2 = pygame.image.load(os.path.join('Assets', 'Background2.png'))
+BACKGROUND3 = pygame.image.load(os.path.join('Assets', 'Background3.png'))
 
-    def run(self):
-        self.image = RUNNING[self.step_index // 5]
-        self.rect.x = self.X_POS
-        self.rect.y = self.Y_POS
-        self.step_index += 1
+GROUND = pygame.image.load(os.path.join('Assets', 'Ground.png'))
+GROUND2 = pygame.image.load(os.path.join('Assets', 'Ground2.png'))
+GROUND3 = pygame.image.load(os.path.join('Assets', 'Ground3.png'))
 
-    def draw(self, SCREEN):
-        SCREEN.blit(self.image, (self.rect.x, self.rect.y))
-        pygame.draw.rect(SCREEN, self.color, (self.rect.x, self.rect.y, self.rect.width, self.rect.height), 2)
-        for obstacle in obstacles:
-            pygame.draw.line(SCREEN, self.color, (self.rect.x + 54, self.rect.y + 12), obstacle.rect.center, 2)
+BORDER = pygame.Rect(0, HEIGHT//2 + 100, WIDTH, 1)
 
-class Obstacles:
-    def __init__(self, image, number_of_cacti):
-        self.image = image
-        self.type = number_of_cacti
-        self.rect = self.image[self.type].get_rect()
-        self.color = (26, 148, 49)
-        self.rect.x = SCREEN_WIDTH
+font = r'C:\Users\deepp\Desktop\VSCodeFiles\Python\Games\DinoAi\Assets\PressStart2P-Regular.ttf'
+SCORE_FONT = pygame.font.Font(font, 12)
 
-    def update(self):
-        self.rect.x -= game_speed
-        if self.rect.x < -self.rect.width:
-            obstacles.pop(0)
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('DINO GAME')
 
-    def draw(self, screen):
-        SCREEN.blit(self.image[self.type], self.rect)
-        pygame.draw.rect(SCREEN, self.color, (self.rect.x, self.rect.y, self.rect.width, self.rect.height), 2)
+class Dino:
+
+    def __init__(self, y):
+        self.x = 15
+        self.y = y
+        self.img = DINO
+        self.jump_vel = 10
+        self.jumping = False
+        self.rounded_score = 1
+        self.actual_score = 1
+        self.animation = 0
+        self.animation_count = 0
 
 
+    def get_mask(self):
+        return pygame.mask.from_surface(self.img)
 
-class SmallCactus(Obstacles):
-    def __init__(self, image, number_of_cacti):
-        super().__init__(image, number_of_cacti)
-        self.rect.y = 325
+    def jump(self):
 
-class LargeCactus(Obstacles):
-    def __init__(self, image, number_of_cacti):
-        super().__init__(image, number_of_cacti)
-        self.rect.y = 300
+        tick_count = 0.5
+        tick_count -= 1/1000
+
+        if self.y >= DINO_Y - 100:
+            if self.y <= DINO_Y - 90:
+                tick_count = 0.2
+            d = tick_count * self.jump_vel + 1.5 * tick_count ** 2
+            self.y -= d
+
+        if self.y >= DINO_Y:
+            self.jump_vel *= -1
+            self.y = DINO_Y
+            return True
+
+        if self.y <= DINO_Y - 100:
+            self.jump_vel *= -1
+            d = tick_count * self.jump_vel + 1.5 * tick_count ** 2
+            self.y -= d
+
+    def draw(self):
+
+        self.animation_count += 1
+
+        if self.y != DINO_Y:
+            WIN.blit(DINO, (self.x, self.y))
+        elif self.animation_count <= 5:
+            WIN.blit(DINO1, (self.x, self.y))
+        elif 5 <= self.animation_count <= 10:
+            WIN.blit(DINO2, (self.x, self.y))
+        else:
+            WIN.blit(DINO2, (self.x, self.y))
+            self.animation_count = 0
 
 
-def remove(index):
-    dinosaurs.pop(index)
-    ge.pop(index)
-    nets.pop(index)
+class Obstacle:
 
-def distance(pos_a, pos_b):
-    dx = pos_a[0] -pos_b[0]
-    dy = pos_a[1] -pos_b[1]
-    return math.sqrt(dx**2+dy**2)
+    obstacle_list = []
+    current_obstacles = []
+    counter = 0
+    right_spawn_border = WIDTH // 2 + 50
+    counter_tick = 0
+    obstacle_vel = 5
 
-def eval_genomes(genomes, config):
-    global game_speed, x_pos_bg, y_pos_bg, obstacles, dinosaurs, points, ge, nets
+    def __init__(self, x, y, width, height, img):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.img = img
+        self.animation_count = 0
+    
+    @classmethod
+    def add_obstacle(cls, dino):
+        if dino.rounded_score >= 500:
+            random_obstacle = random.choice(cls.obstacle_list)
+            if random_obstacle.img == BIRD:
+                random_obstacle.y = random.choice([HEIGHT//2, HEIGHT//2 + 35, DINO_Y + 10])
+
+        else:    
+            random_obstacle = random.choice(cls.obstacle_list[:len(cls.obstacle_list)-1])
+
+        new_obstacle = Obstacle(random_obstacle.x, random_obstacle.y, random_obstacle.width, random_obstacle.height, random_obstacle.img)
+        cls.current_obstacles.append(new_obstacle)
+
+    @classmethod
+    def spawn(cls, dino):
+
+        cls.counter += 1
+ 
+        bottom_counter = random.randint(20, 60) + cls.counter_tick
+
+        if len(cls.current_obstacles) == 0:
+            Obstacle.add_obstacle(dino)
+
+        if dino.rounded_score % 200 == 0 and dino.rounded_score <= 1200:
+            cls.right_spawn_border -= 25/6
+            cls.counter_tick += 10/6
+            cls.obstacle_vel += 1/6
+
+        for current in cls.current_obstacles:
+
+            current.x -= cls.obstacle_vel
+
+            if 50 <= current.x <= cls.right_spawn_border and len(cls.current_obstacles) < 2 and bottom_counter <= cls.counter <= 120:
+                Obstacle.add_obstacle(dino)
+                cls.counter = 0
+            
+            if current.x <= -current.width:
+                cls.current_obstacles.remove(current)
+
+            collided = current.collide(dino)
+
+            if collided:
+                return True
+
+            current.draw()  
+
+
+    def draw(self):
+        if self.img != BIRD:
+            WIN.blit(self.img, (self.x, self.y))
+
+        elif 0 <= self.animation_count <= 15:
+            WIN.blit(BIRD, (self.x, self.y))
+            self.animation_count += 1
+
+        elif 15 <= self.animation_count <= 30:
+            WIN.blit(BIRD2, (self.x, self.y))
+            self.animation_count += 1
+
+        else:
+            WIN.blit(BIRD, (self.x, self.y))
+            self.animation_count = 0
+        
+
+    def collide(self, dino):
+        dino_mask = dino.get_mask()
+        obstacle_mask = pygame.mask.from_surface(self.img)
+
+        obstacle_offset = (round(self.x - dino.x), round(self.y - dino.y))
+
+        point = dino_mask.overlap(obstacle_mask, obstacle_offset)
+
+        if point:
+            return True
+
+        return False
+        
+
+def draw_window(dino, backgrounds, grounds):
+
+    WIN.fill(WHITE)
+
+    WIN.blit(BACKGROUND, (backgrounds[0] ,0))
+    WIN.blit(BACKGROUND2, (backgrounds[1] ,0))
+    WIN.blit(BACKGROUND3, (backgrounds[2] ,0))
+
+    score_text = SCORE_FONT.render(f'{dino.rounded_score}', True, GRAY)
+    WIN.blit(score_text, (WIDTH - score_text.get_width() - 20, 20))
+
+    WIN.blit(GROUND, (grounds[0] ,DINO_Y + 40))
+    WIN.blit(GROUND2, (grounds[1] ,DINO_Y + 40))
+    WIN.blit(GROUND3, (grounds[2] ,DINO_Y + 40))
+
+    if dino.jumping:
+        reached_ground = dino.jump()
+        if reached_ground:
+            dino.jumping = False
+
+    dino.draw()
+
+    collided = Obstacle.spawn(dino)
+
+    if collided:
+        return True
+
+    pygame.display.update()
+
+
+def main():
+
+    WIN.fill(WHITE)
+
+    dino = Dino(DINO_Y)
+    one_cactus_small = Obstacle(750, HEIGHT//2 + 75, ONE_CACTUS_SMALL_WIDTH, ONE_CACTUS_SMALL_HEIGHT, ONE_CACTUS_SMALL)
+    one_cactus_tall = Obstacle(750, HEIGHT//2 + 57, ONE_CACTUS_TALL_WIDTH, ONE_CACTUS_TALL_HEIGHT, ONE_CACTUS_TALL)
+    two_cactus_small = Obstacle(750, HEIGHT//2 + 70, TWO_CACTUS_SMALL_WIDTH, TWO_CACTUS_SMALL_HEIGHT, TWO_CACTUS_SMALL)
+    two_cactus_tall = Obstacle(750, HEIGHT//2 + 56, TWO_CACTUS_TALL_WIDTH, TWO_CACTUS_TALL_HEIGHT, TWO_CACTUS_TALL)
+    four_cactus_small = Obstacle(750, HEIGHT//2 + 70, FOUR_CACTUS_SMALL_WIDTH, FOUR_CACTUS_SMALL_HEIGHT, FOUR_CACTUS_SMALL)
+    four_cactus_tall = Obstacle(750, HEIGHT//2 + 56, FOUR_CACTUS_TALL_WIDTH, FOUR_CACTUS_TALL_HEIGHT, FOUR_CACTUS_TALL)
+    bird = Obstacle(750, HEIGHT//2, BIRD_WIDTH, BIRD_HEIGHT, BIRD)
+
+    Obstacle.obstacle_list = []
+    Obstacle.current_obstacles = []
+    Obstacle.counter = 0
+    Obstacle.obstacle_vel = 5
+
+    Obstacle.obstacle_list.extend([one_cactus_small, one_cactus_tall, two_cactus_small, two_cactus_tall, 
+                                   four_cactus_small, four_cactus_tall, bird])
+
+    bg1x = 0
+    bg2x = BACKGROUND.get_width()
+    bg3x = BACKGROUND2.get_width() * 2
+
+    grd1x = 0
+    grd2x = GROUND.get_width()
+    grd3x = GROUND2.get_width() * 2
+
     clock = pygame.time.Clock()
-    points = 0
-
-    dinosaurs = []
-    obstacles = []
-    ge = []
-    nets = []
-
-    x_pos_bg = 0
-    y_pos_bg = 380
-    game_speed = 20
-
-    for genome_id, genome in genomes:
-        dinosaurs.append(Dinosaur())
-        ge.append(genome)
-        net = neat.nn.FeedForwardNetwork.create(genome, config)
-        nets.append(net)
-        genome.fitness = 0
-
-    def score():
-        global points, game_speed
-        points +=1
-        if points % 100 == 0 and game_speed != 50:
-            game_speed +=1
-        text = FONT.render(f'Points: {str(points)}', True, (0, 0, 0))
-        SCREEN.blit(text, (950, 50))
-
-    def statistics():
-        global dinosaurs, game_speed, ge
-        text_1 = FONT.render(f"Dinosaurs Alive: {str(len(dinosaurs))}", True, (0, 0, 0))
-        text_2 = FONT.render(f"Generation: {pop.generation+1}", True, (0, 0, 0))
-        text_3 = FONT.render(f"Game Speed: {str(game_speed)}", True, (0, 0, 0))
-
-        SCREEN.blit(text_1, (50, 450))
-        SCREEN.blit(text_2, (50, 480))
-        SCREEN.blit(text_3, (50, 510))
-
-    def background():
-        global x_pos_bg, y_pos_bg
-        image_width = BG.get_width()
-        SCREEN.blit(BG, (x_pos_bg, y_pos_bg))
-        SCREEN.blit(BG, (image_width + x_pos_bg, y_pos_bg))
-        if x_pos_bg <= -image_width:
-            x_pos_bg = 0
-        x_pos_bg -= game_speed
-
-
     run = True
+    
     while run:
+
+        clock.tick(FPS)
+
+        bg1x -= BACKGROUND_VEL
+        bg2x -= BACKGROUND_VEL
+        bg3x -= BACKGROUND_VEL
+
+        grd1x -= Obstacle.obstacle_vel
+        grd2x -= Obstacle.obstacle_vel
+        grd3x -= Obstacle.obstacle_vel
+
+
+        if bg1x < BACKGROUND.get_width() * -1:
+            bg1x = BACKGROUND.get_width()
+        if bg2x < BACKGROUND.get_width() * -1:
+            bg2x = BACKGROUND.get_width()
+        if bg3x < BACKGROUND.get_width() * -1:
+            bg3x = BACKGROUND.get_width()
+
+        if grd1x < GROUND.get_width() * -1:
+            grd1x = GROUND.get_width()
+        if grd2x < GROUND.get_width() * -1:
+            grd2x = GROUND.get_width()
+        if grd3x < GROUND.get_width() * -1:
+            grd3x = GROUND.get_width()
+
+        backgrounds = [bg1x, bg2x, bg3x]
+        grounds = [grd1x, grd2x, grd3x]
+        
         for event in pygame.event.get():
+
             if event.type == pygame.QUIT:
+                run = False
                 pygame.quit()
-                sys.exit()
 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and dino.y == DINO_Y:
+                    dino.jumping = True
 
-        SCREEN.fill((255, 255, 255))
-
-        for dinosaur in dinosaurs:
-            dinosaur.update()
-            dinosaur.draw(SCREEN)
-
-        if len(dinosaurs) == 0:
+        dino.actual_score += 5/33
+        dino.rounded_score = round(dino.actual_score)
+        collided = draw_window(dino, backgrounds, grounds)
+        
+        if collided:
             break
-
-        if len(obstacles) == 0:
-            rand_int = random.randint(0, 1)
-            if rand_int == 0:
-                obstacles.append(SmallCactus(SMALL_CACTUS, random.randint(0, 2)))
-            else:
-                obstacles.append(LargeCactus(LARGE_CACTUS, random.randint(0, 2)))
+    
+    main()
 
 
-        for obstacle in obstacles:
-            obstacle.draw(SCREEN)
-            obstacle.update()
-            for i, dinosaur in enumerate(dinosaurs):
-                if dinosaur.rect.colliderect(obstacle.rect):
-                    ge[i].fitness -= 10
-                    remove(i)
-        for i, dinosaur in enumerate(dinosaurs):
-            output = nets[i].activate((dinosaur.rect.y, distance((dinosaur.rect.x, dinosaur.rect.y), obstacle.rect.midtop), game_speed, obstacle.rect.size[0], obstacle.rect.size[1]))
-            print(output)
-            if output[0] >= 0.95 and dinosaur.rect.y == dinosaur.Y_POS:
-                print(output, "high jump")
-                dinosaur.dino_Bigjump = True
-                dinosaur.dino_run = False
-
-
-
-
-        statistics()
-        score()
-        background()
-        clock.tick(30)
-        pygame.display.update()
-
-def run(config_path):
-    global pop
-    config = neat.config.Config(
-        neat.DefaultGenome,
-        neat.DefaultReproduction,
-        neat.DefaultSpeciesSet,
-        neat.DefaultStagnation,
-        config_path)
-
-    pop = neat.Population(config)
-    pop.run(eval_genomes, 50)
-
-
-if __name__ == '__main__':
-    local_dir = os.path.dirname(__file__)
-    config_path = os.path.join(local_dir, 'config.txt')
-    run(config_path)
+if __name__ == "__main__":
+    main()
